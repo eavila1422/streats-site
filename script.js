bf3ef9ced6d6cdefe2c2e1ec659839d9bd3ea276
eavila1422 <eavila1422@gmail.com>
2025-02-22 23:36:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function showForm() {
-  document.getElementById('form').style.display = 'block';
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFRyLHLDumJpteFlannZMcEX3l8VpuQlM",
+  authDomain: "streats-site.firebaseapp.com",
+  projectId: "streats-site",
+  storageBucket: "streats-site.firebasestorage.app",
+  messagingSenderId: "435856449927",
+  appId: "1:435856449927:web:021d6dae14a84320627322",
+  measurementId: "G-S7M1VMZCFR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
